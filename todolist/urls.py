f31170c73ac5112b2cb7a01caa1f0f64ea086bd1
Jonnatas <matiasjonnatas@gmail.com>
2018-07-30 23:51:28 -0300

    Testing with dev settings

--- todolist/urls.py
+++ todolist/urls.py
@@ -15,9 +15,3 @@ urlpatterns = [
     url(r'^', TemplateView.as_view(template_name='home.html'), name='home')
 ]
 
-
-if settings.DEBUG:
-    import debug_toolbar
-    urlpatterns = [
-        url(r'^__debug__/', include(debug_toolbar.urls)),
-    ] + urlpatterns
