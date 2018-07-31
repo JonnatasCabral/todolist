8c321ff5971ef0f57ff1cef94ba539a7a199e565
Jonnatas <matiasjonnatas@gmail.com>
2018-07-31 15:56:49 -0300

    Testing with axios defaults settings

--- assets/js/api/authApi.js
+++ assets/js/api/authApi.js
@@ -1,16 +1,17 @@
 import axios from "axios";
-import getCookie from '../common/helpers'
 import { sucessLogin, sucessLogout} from '../actions/actionAuth';
+
+
+axios.defaults.xsrfHeaderName = "X-CSRFTOKEN";
+axios.defaults.xsrfCookieName = "csrftoken";
 
 
 export const login = (user) => {
   const LOGIN_URL = "/api/v1/rest-auth/login/";
-  const csrf = getCookie('csrftoken')
   const config = {
     headers: {
       'Accept': 'application/json',
       'Content-type': 'application/json',
-      'X-CSRFToken': csrf
     },
 
   }
@@ -27,12 +28,10 @@ export const login = (user) => {
 
 export const logout = () => {
   const LOGOUT_URL = "/api/v1/rest-auth/logout/";
-  const csrf = getCookie('csrftoken')
   const config = {
     headers: {
       'Accept': 'application/json',
       'Content-type': 'application/json',
-      'X-CSRFToken': csrf
     },
 
   }
@@ -50,12 +49,10 @@ export const logout = () => {
 export const createUser = (user) => {
   return (dispatch) => {
     const USER_URL = "/api/v1/users/register/";
-    const csrf = getCookie('csrftoken');
     const config = {
       headers: {
         'Accept': 'application/json',
         'Content-type': 'application/json',
-        'X-CSRFToken': csrf,
       },
     }
     axios.post(`${USER_URL}`, user ,config)
